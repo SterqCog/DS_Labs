0793ace9bc0df127e7605f35b035d81aac1d1368
SterqCog <nerostorm014@gmail.com>
2026-05-16 18:52:37 +0300

    Update matmul_avx512.cpp

--- matmul_avx512.cpp
+++ matmul_avx512.cpp
@@ -10,15 +10,12 @@ void matmul_avx512(int N,
     const int n = N;
     const int BLOCK_SIZE = 64;
 
-    // Переходим на работу с сырыми структурами внутри вектора
     const std::complex<float>* A_flat = A.data();
     const std::complex<float>* B_flat = B.data();
     std::complex<float>* C_flat = C.data();
 
-    // Быстро обнуляем результирующий массив перед накоплением сумм
     std::fill(C_flat, C_flat + n * n, std::complex<float>(0.0f, 0.0f));
 
-    // Маска знаков для ZMM
     __m512 sign_mask = _mm512_set_ps(
         0.0f, -0.0f, 0.0f, -0.0f, 0.0f, -0.0f, 0.0f, -0.0f,
         0.0f, -0.0f, 0.0f, -0.0f, 0.0f, -0.0f, 0.0f, -0.0f
@@ -50,7 +47,6 @@ void matmul_avx512(int N,
                             __m512 vec_b = _mm512_loadu_ps(reinterpret_cast<const float*>(&B_flat[idx_b]));
                             __m512 vec_b_shuf = _mm512_shuffle_ps(vec_b, vec_b, _MM_SHUFFLE(2, 3, 0, 1));
 
-                            // --- Строка C0 ---
                             int idx_c0 = i * n + j;
                             __m512 vec_c0 = _mm512_loadu_ps(reinterpret_cast<float*>(&C_flat[idx_c0]));
                             vec_c0 = _mm512_fmadd_ps(vec_ar0, vec_b, vec_c0);
@@ -59,7 +55,6 @@ void matmul_avx512(int N,
                             vec_c0 = _mm512_add_ps(vec_c0, i_term0);
                             _mm512_storeu_ps(reinterpret_cast<float*>(&C_flat[idx_c0]), vec_c0);
 
-                            // --- Строка C1 ---
                             int idx_c1 = (i + 1) * n + j;
                             __m512 vec_c1 = _mm512_loadu_ps(reinterpret_cast<float*>(&C_flat[idx_c1]));
                             vec_c1 = _mm512_fmadd_ps(vec_ar1, vec_b, vec_c1);
@@ -68,8 +63,7 @@ void matmul_avx512(int N,
                             vec_c1 = _mm512_add_ps(vec_c1, i_term1);
                             _mm512_storeu_ps(reinterpret_cast<float*>(&C_flat[idx_c1]), vec_c1);
 
-                            // --- Строка C2 ---
-                            int idx_c2 = (i + 2) * n + j; // Строка очищена от мусора
+                            int idx_c2 = (i + 2) * n + j;
                             __m512 vec_c2 = _mm512_loadu_ps(reinterpret_cast<float*>(&C_flat[idx_c2]));
                             vec_c2 = _mm512_fmadd_ps(vec_ar2, vec_b, vec_c2);
                             __m512 i_term2 = _mm512_mul_ps(vec_ai2, vec_b_shuf);
@@ -77,7 +71,6 @@ void matmul_avx512(int N,
                             vec_c2 = _mm512_add_ps(vec_c2, i_term2);
                             _mm512_storeu_ps(reinterpret_cast<float*>(&C_flat[idx_c2]), vec_c2);
 
-                            // --- Строка C3 ---
                             int idx_c3 = (i + 3) * n + j;
                             __m512 vec_c3 = _mm512_loadu_ps(reinterpret_cast<float*>(&C_flat[idx_c3]));
                             vec_c3 = _mm512_fmadd_ps(vec_ar3, vec_b, vec_c3);
