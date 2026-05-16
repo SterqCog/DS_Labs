cc322dcd0499cadc118bd529e95ac9406b91aff8
SterqCog <nerostorm014@gmail.com>
2026-05-16 18:50:28 +0300

    Update matmul.h

--- matmul.h
+++ matmul.h
@@ -1,15 +1,12 @@
-// matmul.h
 #pragma once
 #include <vector>
 #include <complex>
 
-// Прототип функции для AVX2
 void matmul_avx2(int n,
     const std::vector<std::complex<float>>& A,
     const std::vector<std::complex<float>>& B,
     std::vector<std::complex<float>>& C);
 
-// Прототип функции для AVX-512
 void matmul_avx512(int n,
     const std::vector<std::complex<float>>& A,
     const std::vector<std::complex<float>>& B,
